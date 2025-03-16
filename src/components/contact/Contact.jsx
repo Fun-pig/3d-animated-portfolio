7886aa7ccaa0d52c4d977dd944584a54c4bee6c0
zhangchaojun <893499851@qq.com>
2025-03-16 21:08:45 +0800

    项目完成

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,100 @@
-import "./contact.css"
+import "./contact.css";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { motion, useInView } from "motion/react";
+import ContactSvg from "./ContactSvg";
+
+const listVariant = {
+  initial: {
+    x: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
 
 const Contact = () => {
-  return (
-    <div className='contact'>Contact</div>
-  )
-}
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const form = useRef();
+  const ref = useRef();
 
-export default Contact
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        form.current,
+        {
+          publicKey: import.meta.env.VITE_PUBLIC_KEY,
+        }
+      )
+      .then(
+        () => {
+          setSuccess(true);
+          setError(false);
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          setError(true);
+          setSuccess(false);
+        }
+      );
+  };
+
+  const isInView = useInView(ref, { margin: "-200px" });
+  return (
+    <div className="contact" ref={ref}>
+      <div className="cSection">
+        <motion.form
+          variants={listVariant}
+          animate={isInView ? "animate" : "initial"}
+          ref={form}
+          onSubmit={sendEmail}
+        >
+          <motion.h1 variants={listVariant} className="cTitle">
+            Let's keep in touch
+          </motion.h1>
+          <motion.div variants={listVariant} className="formItem">
+            <label>Name</label>
+            <input type="text" placeholder="John Doe" name="to_name" />
+          </motion.div>
+          <motion.div variants={listVariant} className="formItem">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="John@gmail.com"
+              name="user_email"
+            />
+          </motion.div>
+          <motion.div variants={listVariant} className="formItem">
+            <label>Message</label>
+            <textarea
+              rows={10}
+              placeholder="Write your message..."
+              name="user_message"
+            ></textarea>
+          </motion.div>
+          <motion.button variants={listVariant} className="formButton">
+            Send
+          </motion.button>
+          {success && <span>Your message has been sent!</span>}
+          {error && <span>Something went wrong!</span>}
+        </motion.form>
+      </div>
+      <div className="cSection">
+        <ContactSvg />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
